68814ed327b737e7ecbe3231aa5d926725562100
ClaytonBudd <claytonbudd@gmail.com>
2024-02-01 13:59:55 -0700

    finished win logic

--- lib/board.rb
+++ lib/board.rb
@@ -3,6 +3,8 @@ class Board
 
   def initialize
     @nodes = {}
+    @diagonal_left_heads = [[3,1],[2,1],[1,1],[1,2],[1,3],[1,4]]
+    @diagonal_right_heads = [[3,7],[2,7],[1,7],[1,6],[1,5],[1,4]]
   end
 
   def add_node(node)
@@ -28,8 +30,6 @@ class Board
     end
   end
 
-
-
   def find_edges(node)
     possible_edges = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
 
@@ -40,7 +40,6 @@ class Board
     legal_edges
   end
    
-
   def display_board
     keys = @nodes.keys
     keys.sort!
@@ -84,18 +83,158 @@ class Board
     true
   end
 
+  def win_row?(row, marker)
+    current = [row, 1]
+    count = 0
+    7.times do
+      if @nodes[current] == marker
+        count += 1
+      else
+        count = 0  
+      end
+
+      current[1] += 1  
+      
+      return true if count == 4
+
+    end
+    false
+  end
+
+  def win_column?(column, marker)
+    current = [1, column]
+    count = 0
+    7.times do
+      if @nodes[current] == marker
+        count +=1
+      else
+        count = 0
+      end
+
+      current[0] +=1
+
+      return true if count ==4
+
+    end
+    false
+  end
+
+  def find_top_marker(column)
+    current = [1, column]
+    while @nodes[current] == "[ ]"
+      current[0] += 1
+    end
+    return current
+  end
+
+  def scan_diag_left(node)
+    stack = @diagonal_left_heads
+    while stack != []
+      head = stack.shift
+      current = [head[0],head[1]]
+      while current[0] < 7 && current[1] < 8
+        if current == node
+          return head
+        else
+          current[0] +=1
+          current[1] +=1
+        end
+      end
+    end
+    return nil
+  end
+
+  def scan_diag_right(node)
+    stack = @diagonal_right_heads
+    while stack != []
+      head = stack.shift
+      current = [head[0],head[1]]
+      while current[0] < 7 && current[1] > 0
+        if current == node
+          return head
+        else
+          current[0] +=1
+          current[1] -=1
+        end
+      end
+    end
+    return nil
+  end
+
+  def win_diagonal_left?(column, marker)
+    node = find_top_marker(column)
+    current = scan_diag_left(node)
+    
+    return false if current == nil
+
+    count = 0
+    while current[0] < 7 && current[1] < 8
+      if @nodes[current] == marker
+        count += 1
+      else
+        count = 0
+      end
+      
+      current[0] +=1
+      current[1] +=1
+
+      return true if count == 4
+
+    end
+    false
+  end
+
+  def win_diagonal_right?(column, marker)
+    node = find_top_marker(column)
+    current = scan_diag_right(node)
+    
+    return false if current == nil
+
+    count = 0
+    while current[0] < 7 && current[1] > 0
+      if @nodes[current] == marker
+        count += 1
+      else
+        count = 0
+      end
+      
+      current[0] +=1
+      current[1] -=1
+
+      return true if count == 4
+
+    end
+    false
+  end
+
 end
 
-board = Board.new
-board.build_board
-board.unmark_board
-#board.display_board
-board.mark_board(1, "W")
-board.mark_board(2, "B")
-board.mark_board(2, "B")
-board.mark_board(3, "W")
-board.mark_board(2, "B")
-board.mark_board(6, "W")
-board.mark_board(6, "B")
-board.mark_board(7, "W")
-board.display_board
+# board = Board.new
+# board.build_board
+# board.unmark_board
+# #board.display_board
+# board.mark_board(1, "B")
+# board.mark_board(2, "B")
+# board.mark_board(1, "B")
+# board.mark_board(2, "B")
+# board.mark_board(3, "W")
+# board.mark_board(4, "W")
+# board.mark_board(3, "W")
+# board.mark_board(4, "W")
+# board.mark_board(5, "B")
+# board.mark_board(4, "B")
+# board.mark_board(3, "B")
+# board.mark_board(3, "W")
+# board.mark_board(4, "B")
+# board.mark_board(3, "W")
+# board.mark_board(6, "W")
+# board.mark_board(3, "W")
+# board.display_board
+# #puts "#{board.win_row?(6, "[W]")}"
+# #puts "#{board.win_column?(6,"[B]")}"
+# #puts board.win_diagonal_left?(7, "[W]")
+# #puts board.win_diagonal_right?(7, "[B]")
+
+
+
+# next put it all together with player turns and test it out.
--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,6 @@
 require_relative "board.rb"
 require_relative "player.rb"
 require_relative "messages.rb"
-require_relative "linked_list"
-require_relative "node.rb"
 
 class Game
   attr_accessor :current_player, :player_1, :player_2
@@ -10,11 +8,13 @@ class Game
 
   def initialize
     @player_1 = Player.new("R", "player_1")
-    @player_2 = Player.new("B", "player_2")
+    @player_2 = Player.new("W", "player_2")
     @current_player = [@player_1, @player_2].sample
     @board = Board.new
     @messages = Messages.new
   end
+
+
 
   
 end
